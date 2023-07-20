bba71107c805161db58bcfe152476cc8f93fe4b8
NaveenPrajapati <solidauto202@gmail.com>
2023-07-20 22:16:58 +0530

    change project

--- src/components/ProjectSection.js
+++ src/components/ProjectSection.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { BsPuzzleFill } from 'react-icons/bs'
 import { AiOutlineGithub } from 'react-icons/ai'
 import { SiNetlify } from 'react-icons/si'
-import projectImg4  from '../assests/projectSnapshot.png'
+
 import projectImg1  from '../assests/robogame.png'
-import projectImg3  from '../assests/CrudApp.png'
+import shoppingImg  from '../assests/shoppingImg.png'
 import projectImg2  from '../assests/projectBlog.png'
 import studynotionImg  from '../assests/studynotionImg.png'
 import ProjectCard from './ProjectCard'
@@ -31,37 +31,37 @@ function ProjectSection() {
 
       {/* second projext */}
       <ProjectCard
-        name={'Blog App'}
+        name={'Ecommerce App'}
         direction={'flex-row'}
+        image={shoppingImg}
+        tech={['React','useState']}
+        gitLink={'https://github.com/NaveenPrajpati/shopping'}
+        liveLink={'https://shopping-chi-ten.vercel.app/'}
+        description={'This is Full-stack Ecommerce app in this app you can sell you product as a seller and any user can buy your prduct you can track your sellings and profits. This app include braintree payment gateway'}
+      />
+     
+    {/* third projext */}
+    <ProjectCard
+        name={'Blog App'}
+        direction={'flex-row-reverse'}
         image={projectImg2}
         tech={['React','Redux-Toolkit']}
         gitLink={'https://github.com/NaveenPrajpati/blog-front'}
         liveLink={'https://naveen-blogapp.netlify.app/'}
         description={'This is Blog app in which you can add your Stories by login into your acccount and you can also like and comment on others post. App has full login authentication features for storing media we using cloudinary server'}
       />
-     
-    {/* third projext */}
-     <ProjectCard
+
+{/* fourth projext */}
+
+  <ProjectCard
         name={'Robot Game'}
-        direction={'flex-row-reverse'}
+        direction={'flex-row'}
         image={projectImg1}
         tech={['React','useState']}
         gitLink={'https://github.com/NaveenPrajpati/assignmentRacho'}
         liveLink={'https://648d3f13b32a794b204664d6--eclectic-flan-f5667d.netlify.app/'}
         description={'This is frontend react base project managing state is backbone of this project with drag and drop functionality you can change state or we can say position of robot in this game'}
       />
-
-{/* fourth projext */}
-<ProjectCard
-        name={'Employee Management App'}
-        direction={'flex-row'}
-        image={projectImg3}
-        tech={['React','useState']}
-        gitLink={'https://github.com/NaveenPrajpati/emp-manage'}
-        liveLink={'https://6470746aa1724172eb3bfbe8--heartfelt-cobbler-b062a3.netlify.app/'}
-        description={'This is simple CRUD base application in which you can add, remove, update, delete employee'}
-      />
-
 
 
     </div>
